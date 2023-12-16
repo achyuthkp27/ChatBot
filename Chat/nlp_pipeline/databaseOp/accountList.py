c9ebbc60fa0d677ce1a0eca644e21736b5910a9d
Achyuth kp <39870982+achyuthkp27@users.noreply.github.com>
2023-12-16 12:30:32 +0530

    Add files via upload

--- Chat/nlp_pipeline/databaseOp/accountList.py
+++ Chat/nlp_pipeline/databaseOp/accountList.py
@@ -1,10 +1,52 @@
+import random
+import time
+
 import psycopg2
 
 from config import config
 
+main_account = {
+                "account_type": "Savings",
+                "account_number": "259944160429",
+                "account_balance": "TTD. 435.00",
+                "account_nickname": "Hira"
+            }
 
-def AccountListConnect():
-    cssStyle = "<p>Here's an overview of your account and current balance</p><br><div class=container><div class=card style=\"background-image: linear-gradient( 109.6deg,  rgba(15,2,2,1) 11.2%, rgba(36,163,190,1) 91.1% );\"><span class=circle></span><div class=top-div><div class=credit-card-text>{}</div><div class=plus-sign><span></span> <span></span></div></div><div class=card-details><div class=card-number>{}</div><div class=balance-text><p>Available balance</p>TTD.<span class=actual-balance>{}</span></div></div></div></div>"
+def mask_account_number(account_number):
+    masked = ''.join(['*' if char.isdigit() else char for char in account_number[:-4]]) + account_number[-4:]
+    return ' '.join(masked[i:i + 4] for i in range(0, len(masked), 4))
+
+
+def filter_accounts(ip, accounts):
+    query = ip.lower().split()
+
+    # Check if the query contains a number (potential account number)
+    potential_numbers = [word for word in query if word.isdigit()]
+
+    # Check if the query contains words related to nicknames
+    potential_nicknames = [word for word in query if
+                           any(word.lower() in acc["account_nickname"].lower() for acc in accounts)]
+
+    # Check if the query contains words related to account types
+    potential_account_types = [word for word in query if
+                               any(word.lower() in acc["account_type"].lower() for acc in accounts)]
+    # Filter accounts based on provided information
+    filtered_accounts = []
+    for acc in accounts:
+        if (any(num in acc["account_number"] for num in potential_numbers)
+                or any(nickname.lower() in acc["account_nickname"].lower() for nickname in potential_nicknames)
+                or any(acc_type.lower() in acc["account_type"].lower() for acc_type in potential_account_types)):
+            filtered_accounts.append(acc)
+    masked_accounts = [
+        {**acc, "account_number": mask_account_number(acc["account_number"])}
+        if "account_number" in acc else acc
+        for acc in (filtered_accounts if filtered_accounts else accounts)
+    ]
+
+    return masked_accounts
+
+
+def AccountListConnect(ip: str):
     connection = None
     try:
         params = config()
@@ -12,14 +54,23 @@ def AccountListConnect():
         connection = psycopg2.connect(**params)
         cursor = connection.cursor()
 
-        select_query: str = "select account_type,account_number,balance from chatbot.account_list where party_id='1058'"
+        select_query: str = "select account_type,account_number,balance,nick_name from chatbot.account_list where party_id='1058'"
         cursor.execute(select_query)
-        records = cursor.fetchone()
-        maskedNum = "**** **** " + records[1][-4:]
+        records = cursor.fetchall()
         print(records)
-        return cssStyle.format(records[0], maskedNum, records[2])
+        accounts = []
+        for row in records:
+            account = {
+                "account_type": row[0],
+                "account_number": row[1],
+                "account_balance": row[2],
+                "account_nickname": row[3]
+            }
+            accounts.append(account)
+        time.sleep(1)
         cursor.close()
-        return cache_records
+
+        return filter_accounts(ip, accounts)
     except(Exception, psycopg2.DatabaseError) as error:
         print(f'Error during database connection: {error}')
     finally:
